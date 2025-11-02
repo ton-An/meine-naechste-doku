a612d5518928869a1394923422c6c5301765245d
Anton <anton@antons-webfabrik.eu>
2025-11-02 14:24:19 +0100

    feat: add en localization

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -1,5 +1,30 @@
 import type { MessageSchema } from './schema'
 
-const en: MessageSchema = {}
+const en: MessageSchema = {
+  genres: {
+    all: 'All',
+    knowledge: 'Knowledge',
+    society: 'Society',
+    history: 'History',
+    nature: 'Nature',
+    sports: 'Sports',
+    politics: 'Politics',
+    travel: 'Travel',
+    culture: 'Culture',
+    trueCrime: 'True Crime',
+    nutrition: 'Nutrition',
+    economy: 'Economy',
+    music: 'Music',
+    stars: 'Stars',
+    health: 'Health',
+    environment: 'Environment',
+    education: 'Education',
+    architecture: 'Architecture',
+    royals: 'Royals',
+    mystery: 'Mystery',
+    drama: 'Drama',
+    entertainment: 'Entertainment',
+  },
+}
 
 export default en
