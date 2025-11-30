ac85fb36ab6af5ae05929606720694b1bd5ef45d
Anton <anton@antons-webfabrik.eu>
2025-11-30 13:24:20 +0100

    feat: add splash view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import SplashView from '@/views/SplashView.vue'
+
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
@@ -7,8 +9,15 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'splash',
+      component: SplashView,
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: HomeView,
+        },
+      ],
     },
   ],
 })
---
+++ src/views/SplashView.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+
+import ZdfAnimatedLogo from '@/components/ZdfAnimatedLogo.vue'
+</script>
+
+<template>
+  <div>
+    <div
+      class="pointer-events-none absolute top-0 left-0 w-full h-full flex justify-center items-center z-100 splash"
+    >
+      <ZdfAnimatedLogo class="w-40" />
+    </div>
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+.splash {
+  opacity: 0;
+  background-color: #ffffff;
+  animation: opacity-fade-out 5.5s ease-in-out;
+}
+
+@keyframes opacity-fade-out {
+  from,
+  34% {
+    opacity: 1;
+  }
+
+  60%,
+  to {
+    opacity: 0;
+  }
+}
+</style>
